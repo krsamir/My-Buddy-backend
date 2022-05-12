9f965da792e9a4a75b32db8f19bbffd6039de8bd
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2022-05-12 16:50:43 +0530

    Name Change

--- src/Router/loginRoutes.js
+++ src/Router/loginRoutes.js
@@ -3,5 +3,5 @@ import loginController from "../Controller/loginController.js";
 const router = express.Router();
 
 router.get("/github/accesstoken", loginController.getGithubAccessToken);
-router.post("/bitbucket/accestoken", loginController.bitBucketAccessToken);
+router.post("/jira/accestoken", loginController.jiraAccessToken);
 export default router;
